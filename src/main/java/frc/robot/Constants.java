5500600131ed81fd26b81da2e1630f80f8f19d36
elec² <hawthornemerald100@gmail.com>
2025-03-01 18:00:47 +0800

    Update right motor canid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
   }
   public static class ElevatorConstants {
     public static final int leftMotor = 6;
-    public static final int rightMotor = 7;
+    public static final int rightMotor = 11;
     public static final double kP = 0.05;
     public static final double kD = 0.0;
     public static final double kI = 0.0;

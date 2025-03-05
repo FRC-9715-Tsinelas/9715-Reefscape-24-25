db1101dfbdc694e822373da9acfb31f1f127773d
Sri-SriPod <srisripod123@gmail.com>
2025-03-05 20:01:50 +0800

    ewrewrwer

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -63,7 +63,7 @@ public class IntakeSubsystem extends SubsystemBase {
         mLeftMotor.set(IntakeConstants.intakeStowCoralSpeed);
         mRightMotor.set(IntakeConstants.intakeStowCoralSpeed);
       }
-      
+    }
   }
 
 

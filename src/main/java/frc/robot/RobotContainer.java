ee4ef03b1baab48aa636902a3d00e8b6092a5fe4
Hawthorn Emerald <hawthornemerald100@gmail.com>
2025-03-05 21:42:26 +0800

    auto chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,6 +45,9 @@ public class RobotContainer {
     configureBindings();
     autoChooser.setDefaultOption("Slow Auto 0.3 for 2 sec", slowAuto);
     autoChooser.addOption("Fast Auto 0.5 for 2 sec", fastAuto);
+    autoChooser.addOption("Starting pos MIDDLE", Autos.autoMid(driveSubsystem, elevatorSubsystem));
+    autoChooser.addOption("Starting pos LEFT", Autos.autoLeft(driveSubsystem, elevatorSubsystem));
+    autoChooser.addOption("Starting pos RIGHT", Autos.autoRight(driveSubsystem, elevatorSubsystem));
     SmartDashboard.putData("chooser boozer", autoChooser);
     SmartDashboard.putNumber("Battery Voltage", RobotController.getBatteryVoltage());
 
